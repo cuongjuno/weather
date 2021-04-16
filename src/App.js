8c1d25b516bbff0b0660bac06d6bca87d2a65400
juno <cuongjunocn1@gmail.com>
2021-04-16 13:17:19 +0700

    init structure project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <></>
+  );
+}
+
+export default App;
---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+export const DOMAIN_SERVER = 'api.openweathermap.org/data/2.5/weather?';
+export const API_KEY = 'b73b624979a3ffaa51d0475f04bccd85';
