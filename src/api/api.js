bbceeb12dca04ae630aa67c148d6e65e772cbebf
juno <cuongjunocn1@gmail.com>
2021-04-16 13:21:27 +0700

    fix structure

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,2 @@
 import axios from 'axios';
 import { DOMAIN_SERVER, API_KEY } from '../utils/config';
-
-export async function getWeatherByCityName(cityName) {
-  return await axios
-    .get(`http://${DOMAIN_SERVER}`, {
-      params: {
-        q: cityName,
-        appid: API_KEY,
-      },
-    })
-    .then((res) => res.data)
-    .catch((e) => console.error(e));
-}
