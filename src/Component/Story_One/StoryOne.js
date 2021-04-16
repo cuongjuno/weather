68e776d971f0c775f62c8b9b0686593d9c83e39f
juno <cuongjunocn1@gmail.com>
2021-04-16 22:40:10 +0700

    update story one => story two

--- src/Component/Story_One/StoryOne.js
+++ src/Component/Story_One/StoryOne.js
@@ -3,13 +3,69 @@ import React, { useState, useEffect } from 'react';
 import './storyOne.styles.scss';
 import listCity from '../../resources/json/city.list.min.json';
 import { getOneCallDailyByCityName } from '../../api/api';
+import _ from 'lodash';
+import axios from 'axios';
+import { DOMAIN_SERVER, API_KEY } from '../../utils/config';
 import moment from 'moment';
-import { Layout, List, Card, Typography, Row, Col, Divider } from 'antd';
+import {
+  Layout,
+  List,
+  Card,
+  Typography,
+  Row,
+  Col,
+  Divider,
+  Select,
+  Modal,
+  Input,
+} from 'antd';
+const { Option } = Select;
 const { Header, Content } = Layout;
 const { Title } = Typography;
 function StoryOne() {
-  const [listQueryCity, setListQueryCity] = useState(listCity.slice(0, 8));
+  const [listQueryCity, setListQueryCity] = useState('');
   const [listWeatherData, setListWeatherData] = useState([]);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [addCity, setAddCity] = useState({});
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setListWeatherData((preState) => {
+      const updateList = preState.filter((item) => item !== 'add');
+      updateList.push(addCity, 'add');
+      localStorage.setItem(
+        'dataCityBackup',
+        JSON.stringify(updateList.filter((item) => item))
+      );
+      console.log(JSON.parse(localStorage.getItem('dataCityBackup')));
+      return updateList;
+    });
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  async function onChange(value) {
+    const query = JSON.parse(value);
+    await axios
+      .get(`http://${DOMAIN_SERVER}/onecall`, {
+        params: {
+          lat: query.lat,
+          lon: query.lon,
+          exclude: 'current,minutely,hourly,alerts',
+          appid: API_KEY,
+        },
+      })
+      .then((res) => {
+        setAddCity({ ...res.data, name: query.name });
+      })
+      .catch((e) => console.error(e));
+  }
+
   const fetchData = async () => {
     Promise.all(
       listQueryCity.map((e) => {
@@ -22,11 +78,11 @@ function StoryOne() {
     ).then((res) => setListWeatherData([...res, 'add']));
   };
   useEffect(() => {
-    fetchData();
+    if (localStorage.getItem('dataCityBackup'))
+      setListWeatherData(JSON.parse(localStorage.getItem('dataCityBackup')));
+    else setListWeatherData(['add']);
+    // fetchData();
   }, []);
-  useEffect(() => {
-    console.log(listWeatherData);
-  }, [listWeatherData]);
 
   return (
     <>
@@ -36,7 +92,7 @@ function StoryOne() {
         </Header>
         <Layout>
           <Content>
-            <Title level={2}>h2. Ant Design</Title>
+            <Title level={2}>Dashboard</Title>
             <List
               grid={{
                 gutter: 16,
@@ -81,7 +137,7 @@ function StoryOne() {
                     </Card>
                   </List.Item>
                 ) : (
-                  <Col>
+                  <Col onClick={showModal}>
                     <Card className="add-city">+ Add city</Card>
                   </Col>
                 )
@@ -90,6 +146,34 @@ function StoryOne() {
           </Content>
         </Layout>
       </Layout>
+      <Modal
+        title="Choose a city"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Select
+          showSearch
+          style={{ width: 320 }}
+          placeholder="City name"
+          optionFilterProp="children"
+          onChange={onChange}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          {_.map(listCity.slice(0, 1000), (e) => (
+            <Option
+              key={e.id}
+              value={`{ "lat":"${e.coord.lat}", "lon":"${e.coord.lon}", "name":"${e.name}" }`}
+            >
+              {e.name}
+            </Option>
+          ))}
+        </Select>
+        ,
+      </Modal>
+      ;
     </>
   );
 }
