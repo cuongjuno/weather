d9659e1958ce7e124e938774479305e6b3bb9b54
juno <cuongjunocn1@gmail.com>
2021-04-16 19:26:58 +0700

    fix  config api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,16 @@
 import axios from 'axios';
 import { DOMAIN_SERVER, API_KEY } from '../utils/config';
+
+export async function getOneCallDailyByCityName(query) {
+  return await axios
+    .get(`http://${DOMAIN_SERVER}/onecall`, {
+      params: {
+        lat: query.lat,
+        lon: query.lon,
+        exclude: 'current,minutely,hourly,alerts',
+        appid: API_KEY,
+      },
+    })
+    .then((res) => res.data)
+    .catch((e) => console.error(e));
+}
