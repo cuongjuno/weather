08bac2c249195da16457613ad432fc58c9a2b7f0
juno <cuongjunocn1@gmail.com>
2021-04-16 22:52:12 +0700

    config routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 import 'antd/dist/antd.css';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import routes from './routes';
 function App() {
+  const showContentMenu = (routes) => {
+    var result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        );
+      });
+    }
+    return result;
+  };
+
   return (
-    <></>
+    <Router>
+      <Switch>{showContentMenu(routes)}</Switch>
+    </Router>
   );
 }
 
